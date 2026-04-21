6123abb1c0cb33014cbf98a8e6cf05b39ac8f339
collinstewart30 <collin.stewart30@yahoo.com>
2026-04-21 13:01:41 -0400

    ga4 fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
+interface Window {
+  dataLayer: any[];
+}
+
+declare function gtag(...args: any[]): void;
